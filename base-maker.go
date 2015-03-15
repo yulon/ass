75f34a984ad4aaf6c98812044ee2f28faf9508e8
Yuchen Wang <yulon@me.com>
2015-03-16 02:57:50 +0800

    Rich write method

--- base-maker.go
+++ base-maker.go
@@ -5,9 +5,10 @@ import (
 )
 
 type baseMaker struct{
-	*os.File
+	f *os.File
 	marks map[string]int64
 	pits []pit
+	next int64
 }
 
 type pit struct{
@@ -24,19 +25,76 @@ func NewBaseMaker(path string) (*baseMaker, error) {
 		return nil, err
 	}
 	return &baseMaker{
-		File: f,
+		f: f,
 		marks: map[string]int64{},
 		pits: []pit{},
 	}, err
 }
 
-func (bm *baseMaker) Mark(key string) {
-	bm.marks[key] = bm.Next()
+func (bm *baseMaker) Write(data []byte) (int64, error) {
+	l, e := bm.f.Write(data)
+	if e != nil {
+		return 0, e
+	}
+	l64 := int64(l)
+	bm.next += l64
+	return l64, nil
 }
 
-func (bm *baseMaker) Next() int64 {
-	fi, _ := bm.Stat()
-	return fi.Size()
+func (bm *baseMaker) WriteString(data string) (int64, error) {
+	return bm.Write([]byte(data))
+}
+
+func (bm *baseMaker) WriteInt8(n int64) (int64, error) {
+	i := int8(n)
+	return bm.Write([]byte{byte(i)})
+}
+
+func (bm *baseMaker) WriteInt16(n int64) (int64, error) {
+	i := int16(n)
+	return bm.Write([]byte{byte(i), byte(i >> 8)})
+}
+
+func (bm *baseMaker) WriteInt32(n int64) (int64, error) {
+	i := int32(n)
+	return bm.Write([]byte{byte(i), byte(i >> 8), byte(i >> 16), byte(i >> 24)})
+}
+
+func (bm *baseMaker) WriteInt64(i int64) (int64, error) {
+	return bm.Write([]byte{byte(i), byte(i >> 8), byte(i >> 16), byte(i >> 24), byte(i >> 32), byte(i >> 40), byte(i >> 48), byte(i >> 56)})
+}
+
+func (bm *baseMaker) writeAt(data []byte, offset int64) (int64, error) {
+	l, e := bm.f.WriteAt(data, offset)
+	if e != nil {
+		return 0, e
+	}
+	l64 := int64(l)
+	bm.next += l64
+	return l64, nil
+}
+
+func (bm *baseMaker) writeInt8At(n int64, offset int64) (int64, error) {
+	i := int8(n)
+	return bm.writeAt([]byte{byte(i)}, offset)
+}
+
+func (bm *baseMaker) writeInt16At(n int64, offset int64) (int64, error) {
+	i := int16(n)
+	return bm.writeAt([]byte{byte(i), byte(i >> 8)}, offset)
+}
+
+func (bm *baseMaker) writeInt32At(n int64, offset int64) (int64, error) {
+	i := int32(n)
+	return bm.writeAt([]byte{byte(i), byte(i >> 8), byte(i >> 16), byte(i >> 24)}, offset)
+}
+
+func (bm *baseMaker) writeInt64At(i int64, offset int64) (int64, error) {
+	return bm.writeAt([]byte{byte(i), byte(i >> 8), byte(i >> 16), byte(i >> 24), byte(i >> 32), byte(i >> 40), byte(i >> 48), byte(i >> 56)}, offset)
+}
+
+func (bm *baseMaker) Mark(key string) {
+	bm.marks[key] = bm.next
 }
 
 const(
@@ -48,7 +106,7 @@ const(
 
 func (bm *baseMaker) WriteRelative(startMark string, endMark string, offset int64, bit uint8) error {
 	bm.pits = append(bm.pits, pit{
-		addr: bm.Next(),
+		addr: bm.next,
 		start: startMark,
 		end: endMark,
 		offset: offset,
@@ -78,24 +136,17 @@ func (bm *baseMaker) Close() error {
 			end = bm.marks[bm.pits[i].end]
 		}
 
-		i64 := end - start + bm.pits[i].offset
-		var data []byte
+		n := end - start + bm.pits[i].offset
 		switch bm.pits[i].bit {
 			case BIT_8:
-				i8 := int8(i64)
-				data = []byte{byte(i8)}
+				bm.writeInt8At(n, bm.pits[i].addr)
 			case BIT_16:
-				i16 := int8(i64)
-				data = []byte{byte(i16), byte(i16 >> 8)}
+				bm.writeInt16At(n, bm.pits[i].addr)
 			case BIT_32:
-				i32 := int8(i64)
-				data = []byte{byte(i32), byte(i32 >> 8), byte(i32 >> 16), byte(i32 >> 24)}
+				bm.writeInt32At(n, bm.pits[i].addr)
 			case BIT_64:
-				data = []byte{byte(i64), byte(i64 >> 8), byte(i64 >> 16), byte(i64 >> 24),
-					byte(i64 >> 32), byte(i64 >> 40), byte(i64 >> 48), byte(i64 >> 56),
-				}
+				bm.writeInt64At(n, bm.pits[i].addr)
 		}
-		bm.WriteAt(data, bm.pits[i].addr)
 	}
-	return bm.File.Close()
+	return bm.f.Close()
 }
