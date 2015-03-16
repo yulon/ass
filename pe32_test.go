5e15666d0622b519cea4159776774b08b1521c7b
Yuchen Wang <yulon@me.com>
2015-03-16 15:56:17 +0800

    Successfully generated PE32

---
+++ pe32_test.go
@@ -0,0 +1,19 @@
+package ass
+
+import (
+	"testing"
+)
+
+func Test_PE32(t *testing.T) {
+	exe, _ := NewPEMaker("pe32_test.exe")
+	exe.WriteDOSHeader()
+	exe.WriteNTHeader()
+	exe.WriteSectionHeader(".one")
+	exe.SectionStart()
+	exe.Import("kernel32.dll", "ExitProcess")
+	exe.Import("msvcrt.dll", "printf")
+	exe.WriteSpace(100)
+	exe.WriteImportDescriptors()
+	exe.SectionEnd()
+	exe.Close()
+}
