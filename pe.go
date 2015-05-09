16ff42b6754c8701c4f2ef1f9b657070a71bf2b1
Yuchen Wang <yulon@me.com>
2015-05-10 04:35:55 +0800

    fuuuuuu...gofmt

--- pe.go
+++ pe.go
@@ -100,11 +100,11 @@ func (pe *PE) writeFileHeader() { // 20字节
 	case AMD64:
 		pe.f.Write(Num16L(pe_IMAGE_FILE_MACHINE_AMD64)) // Machine
 	}
-	pe.f.Write(Num16L(1))                                                                                                                                                                        // NumberOfSections
-	pe.f.Write(Num32L(time.Now().Unix()))                                                                                                                                                        // TimeDateStamp
-	pe.f.Write(Num32L(0))                                                                                                                                                                        // PointerToSymbolTable
-	pe.f.Write(Num32L(0))                                                                                                                                                                        // NumberOfSymbols
-	pe.l.PitOffset("PE.OptionalHeaderStart", "PE.OptionalHeaderEnd", 0, Num16L)                                                                                                                  // SizeOfOptionalHeader
+	pe.f.Write(Num16L(1)) // NumberOfSections
+	pe.f.Write(Num32L(time.Now().Unix())) // TimeDateStamp
+	pe.f.Write(Num32L(0)) // PointerToSymbolTable
+	pe.f.Write(Num32L(0)) // NumberOfSymbols
+	pe.l.PitOffset("PE.OptionalHeaderStart", "PE.OptionalHeaderEnd", 0, Num16L) // SizeOfOptionalHeader
 	pe.f.Write(Num16L(pe_IMAGE_FILE_EXECUTABLE_IMAGE | pe_IMAGE_FILE_LINE_NUMS_STRIPPED | pe_IMAGE_FILE_LOCAL_SYMS_STRIPPED | pe_IMAGE_FILE_LARGE_ADDRESS_AWARE | pe_IMAGE_FILE_DEBUG_STRIPPED)) // Characteristics
 }
 
@@ -116,47 +116,47 @@ func (pe *PE) writeOptionalHeader() {
 	case AMD64:
 		pe.f.Write(Num16L(pe_IMAGE_NT_OPTIONAL_HDR64_MAGIC)) // Magic
 	}
-	pe.f.Write(Num8(1))                                           // MajorLinkerVersion
-	pe.f.Write(Num8(0))                                           // MinerLinkerVersion
+	pe.f.Write(Num8(1)) // MajorLinkerVersion
+	pe.f.Write(Num8(0)) // MinerLinkerVersion
 	pe.l.PitOffset("PE.SectionStart", "PE.SectionEnd", 0, Num32L) // SizeOfCode
-	pe.f.Write(Num32L(0))                                         // SizeOfInitializedData
-	pe.f.Write(Num32L(0))                                         // SizeOfUnInitializedData
-	pe.f.Write(Num32L(pe_RVA_SECTION))                            // AddressOfEntryPoint
-	pe.f.Write(Num32L(pe_RVA_SECTION))                            // BaseOfCode
+	pe.f.Write(Num32L(0)) // SizeOfInitializedData
+	pe.f.Write(Num32L(0)) // SizeOfUnInitializedData
+	pe.f.Write(Num32L(pe_RVA_SECTION)) // AddressOfEntryPoint
+	pe.f.Write(Num32L(pe_RVA_SECTION)) // BaseOfCode
 	if pe.cpu == I386 {
 		pe.f.Write(Num32L(pe_RVA_SECTION)) // BaseOfData
 	}
-	pe.f.Write(pe.nbo(pe.imgBase))                                                  // ImageBase
-	pe.f.Write(Num32L(pe_ALIGNMENT_IMAGE))                                          // SectionAlignment
-	pe.f.Write(Num32L(pe_ALIGNMENT_FILE))                                           // FileAlignment
-	pe.f.Write(Num16L(5))                                                           // MajorOperatingSystemVersion
-	pe.f.Write(Num16L(1))                                                           // MinorOperatingSystemVersion
-	pe.f.Write(Num16L(0))                                                           // MajorImageVersion
-	pe.f.Write(Num16L(0))                                                           // MinorImageVersion
-	pe.f.Write(Num16L(5))                                                           // MajorSubsystemVersion
-	pe.f.Write(Num16L(1))                                                           // MinorSubsystemVersion
-	pe.f.Write(Num32L(0))                                                           // Win32VersionValue
+	pe.f.Write(pe.nbo(pe.imgBase)) // ImageBase
+	pe.f.Write(Num32L(pe_ALIGNMENT_IMAGE)) // SectionAlignment
+	pe.f.Write(Num32L(pe_ALIGNMENT_FILE)) // FileAlignment
+	pe.f.Write(Num16L(5)) // MajorOperatingSystemVersion
+	pe.f.Write(Num16L(1)) // MinorOperatingSystemVersion
+	pe.f.Write(Num16L(0)) // MajorImageVersion
+	pe.f.Write(Num16L(0)) // MinorImageVersion
+	pe.f.Write(Num16L(5)) // MajorSubsystemVersion
+	pe.f.Write(Num16L(1)) // MinorSubsystemVersion
+	pe.f.Write(Num32L(0)) // Win32VersionValue
 	pe.l.PitOffset("PE.SectionStart", "PE.SectionAlignEnd", pe_RVA_SECTION, Num32L) // SizeOfImage
-	pe.l.PitPointer("PE.SectionStart", Num32L)                                      // SizeOfHeaders
-	pe.f.Write(Num32L(0))                                                           // CheckSum
+	pe.l.PitPointer("PE.SectionStart", Num32L) // SizeOfHeaders
+	pe.f.Write(Num32L(0)) // CheckSum
 	if pe.cui {
 		pe.f.Write(Num16L(pe_IMAGE_SUBSYSTEM_WINDOWS_CUI)) // Subsystem
 	} else {
 		pe.f.Write(Num16L(pe_IMAGE_SUBSYSTEM_WINDOWS_GUI)) // Subsystem
 	}
-	pe.f.Write(Num16L(0))     // DllCharacteristics
+	pe.f.Write(Num16L(0)) // DllCharacteristics
 	pe.f.Write(pe.nbo(65536)) // SizeOfStackReserve
-	pe.f.Write(pe.nbo(4096))  // SizeOfStackCommit
+	pe.f.Write(pe.nbo(4096)) // SizeOfStackCommit
 	pe.f.Write(pe.nbo(65536)) // SizeOfHeapReserve
-	pe.f.Write(pe.nbo(4096))  // SizeOfHeapCommit
-	pe.f.Write(Num32L(0))     // LoaderFlags
-	pe.f.Write(Num32L(16))    // NumberOfRvaAndSizes
+	pe.f.Write(pe.nbo(4096)) // SizeOfHeapCommit
+	pe.f.Write(Num32L(0)) // LoaderFlags
+	pe.f.Write(Num32L(16)) // NumberOfRvaAndSizes
 
 	for i := 0; i < 16; i++ {
-		// IMAGE_DATA_DIRECTORY
+		 // IMAGE_DATA_DIRECTORY
 		if i == pe_IMAGE_DIRECTORY_ENTRY_IMPORT {
 			pe.pitRVA("PE.ImportDescriptors", Num32L) // VirtualAddress
-			pe.f.Write(Num32L(40))                    // Size
+			pe.f.Write(Num32L(40)) // Size
 		} else {
 			pe.f.Write(Num32L(0)) // VirtualAddress
 			pe.f.Write(Num32L(0)) // Size
@@ -166,15 +166,15 @@ func (pe *PE) writeOptionalHeader() {
 }
 
 func (pe *PE) writeSectionHeader() error {
-	pe.f.Write(Cstr64(".codata"))                                                                                                    // Name
-	pe.l.PitOffset("PE.SectionStart", "PE.SectionEnd", 0, Num32L)                                                                    // VirtualSize
-	pe.f.Write(Num32L(pe_RVA_SECTION))                                                                                               // VirtualAddress
-	pe.l.PitOffset("PE.SectionStart", "PE.SectionAlignEnd", 0, Num32L)                                                               // SizeOfRawData
-	pe.l.PitPointer("PE.SectionStart", Num32L)                                                                                       // PointerToRawData
-	pe.f.Write(Num32L(0))                                                                                                            // PointerToRelocations
-	pe.f.Write(Num32L(0))                                                                                                            // PointerToLinenumbers
-	pe.f.Write(Num16L(0))                                                                                                            // NumberOfRelocations
-	pe.f.Write(Num16L(0))                                                                                                            // NumberOfLinenumbers
+	pe.f.Write(Cstr64(".codata")) // Name
+	pe.l.PitOffset("PE.SectionStart", "PE.SectionEnd", 0, Num32L) // VirtualSize
+	pe.f.Write(Num32L(pe_RVA_SECTION)) // VirtualAddress
+	pe.l.PitOffset("PE.SectionStart", "PE.SectionAlignEnd", 0, Num32L) // SizeOfRawData
+	pe.l.PitPointer("PE.SectionStart", Num32L) // PointerToRawData
+	pe.f.Write(Num32L(0)) // PointerToRelocations
+	pe.f.Write(Num32L(0)) // PointerToLinenumbers
+	pe.f.Write(Num16L(0)) // NumberOfRelocations
+	pe.f.Write(Num16L(0)) // NumberOfLinenumbers
 	pe.f.Write(Num32L(pe_IMAGE_SCN_CNT_CODE | pe_IMAGE_SCN_MEM_EXECUTE | pe_IMAGE_SCN_MEM_READ | pe_IMAGE_SCN_CNT_INITIALIZED_DATA)) // Characteristics
 	return nil
 }
@@ -205,9 +205,9 @@ func (pe *PE) writeImportDescriptors() {
 	pe.l.Label("PE.ImportDescriptors")
 	for dll, _ := range pe.imps { // 输出 IMAGE_IMPORT_DESCRIPTOR 数组
 		pe.pitRVA("DLL."+dll+".Thunk", Num32L) // OriginalFirstThunk
-		pe.f.Write(Num32L(0))                  // TimeDateStamp
-		pe.f.Write(Num32L(0))                  // ForwarderChain
-		pe.pitRVA("DLL."+dll+".Name", Num32L)  // Name
+		pe.f.Write(Num32L(0)) // TimeDateStamp
+		pe.f.Write(Num32L(0)) // ForwarderChain
+		pe.pitRVA("DLL."+dll+".Name", Num32L) // Name
 		pe.pitRVA("DLL."+dll+".Thunk", Num32L) // FirstThunk
 	}
 	pe.f.Write(Zeros(pe_IMPORT_DESCRIPTOR_SIZE)) // 尾 IMAGE_IMPORT_DESCRIPTOR
@@ -273,7 +273,7 @@ const (
 	pe_IMAGE_FILE_LOCAL_SYMS_STRIPPED = 0x0008 // 不存在符号信息
 	pe_IMAGE_FILE_AGGRESIVE_WS_TRIM   = 0x0010 // 让操作系统强制整理工作区
 	pe_IMAGE_FILE_LARGE_ADDRESS_AWARE = 0x0020 // 应用程序可以处理大于2GB的地址空间
-	//	pe_IMAGE_FILE_??? = 64 // 保留，留以后扩展
+	 //	pe_IMAGE_FILE_??? = 64 // 保留，留以后扩展
 	pe_IMAGE_FILE_BYTES_REVERSED_LO       = 0x0080 // 小尾方式
 	pe_IMAGE_FILE_32BIT_MACHINE           = 0x0100 // 只在32位平台上运行
 	pe_IMAGE_FILE_DEBUG_STRIPPED          = 0x0200 // 不包含调试信息。调试信息位于一个 .DBG 文件中
@@ -284,16 +284,16 @@ const (
 	pe_IMAGE_FILE_UP_SYSTEM_ONLY          = 0x4000 // 只能在单处理器机器中运行
 	pe_IMAGE_FILE_BYTES_REVERSED_HI       = 0x8000 // 大尾方式
 
-	pe_IMAGE_DIRECTORY_ENTRY_EXPORT       = 0  // 指向导出表（IMAGE_EXPORT_DIRECTORY）
-	pe_IMAGE_DIRECTORY_ENTRY_IMPORT       = 1  // 指向导入表（IMAGE_IMPORT_DESCRIPTOR 数组）
-	pe_IMAGE_DIRECTORY_ENTRY_RESOURCE     = 2  // 指向资源（IMAGE_RESOURCE_DIRECTORY）
-	pe_IMAGE_DIRECTORY_ENTRY_EXCEPTION    = 3  // 指向异常处理表（IMAGE_RUNTIME_FUNCTION_ENTRY 数组）。CPU特定的并且基于表的异常处理。用于除x86之外的其它CPU上。
-	pe_IMAGE_DIRECTORY_ENTRY_SECURITY     = 4  // 指向一个 WIN_CERTIFICATE 结构的列表，它定义在 WinTrust.H 中。不会被映射到内存中。因此，VirtualAddress 域是一个文件偏移，而不是一个RVA。
-	pe_IMAGE_DIRECTORY_ENTRY_BASERELOC    = 5  // 指向基址重定位信息
-	pe_IMAGE_DIRECTORY_ENTRY_DEBUG        = 6  // 指向一个 IMAGE_DEBUG_DIRECTORY 结构数组，其中每个结构描述了映像的一些调试信息。早期的 Borland 链接器设置这个 IMAGE_DATA_DIRECTORY 结构的 Size 域为结构的数目，而不是字节大小。要得到 IMAGE_DEBUG_DIRECTORY 结构的数目，用 IMAGE_DEBUG_DIRECTORY 的大小除以这个 Size 域
-	pe_IMAGE_DIRECTORY_ENTRY_ARCHITECTURE = 7  // 指向特定架构数据，它是一个 IMAGE_ARCHITECTURE_HEADER 结构数组。不用于 x86 或 x64，但看来已用于 DEC/Compaq Alpha。
-	pe_IMAGE_DIRECTORY_ENTRY_GLOBALPTR    = 8  // 在某些架构体系上 VirtualAddress 域是一个 RVA，被用来作为全局指针（gp）。不用于 x86，而用于 IA-64。Size 域没有被使用。参见2000年11月的 Under The Hood 专栏可得到关于 IA-64 gp 的更多信息
-	pe_IMAGE_DIRECTORY_ENTRY_TLS          = 9  // 指向线程局部存储初始化节
+	pe_IMAGE_DIRECTORY_ENTRY_EXPORT       = 0 // 指向导出表（IMAGE_EXPORT_DIRECTORY）
+	pe_IMAGE_DIRECTORY_ENTRY_IMPORT       = 1 // 指向导入表（IMAGE_IMPORT_DESCRIPTOR 数组）
+	pe_IMAGE_DIRECTORY_ENTRY_RESOURCE     = 2 // 指向资源（IMAGE_RESOURCE_DIRECTORY）
+	pe_IMAGE_DIRECTORY_ENTRY_EXCEPTION    = 3 // 指向异常处理表（IMAGE_RUNTIME_FUNCTION_ENTRY 数组）。CPU特定的并且基于表的异常处理。用于除x86之外的其它CPU上。
+	pe_IMAGE_DIRECTORY_ENTRY_SECURITY     = 4 // 指向一个 WIN_CERTIFICATE 结构的列表，它定义在 WinTrust.H 中。不会被映射到内存中。因此，VirtualAddress 域是一个文件偏移，而不是一个RVA。
+	pe_IMAGE_DIRECTORY_ENTRY_BASERELOC    = 5 // 指向基址重定位信息
+	pe_IMAGE_DIRECTORY_ENTRY_DEBUG        = 6 // 指向一个 IMAGE_DEBUG_DIRECTORY 结构数组，其中每个结构描述了映像的一些调试信息。早期的 Borland 链接器设置这个 IMAGE_DATA_DIRECTORY 结构的 Size 域为结构的数目，而不是字节大小。要得到 IMAGE_DEBUG_DIRECTORY 结构的数目，用 IMAGE_DEBUG_DIRECTORY 的大小除以这个 Size 域
+	pe_IMAGE_DIRECTORY_ENTRY_ARCHITECTURE = 7 // 指向特定架构数据，它是一个 IMAGE_ARCHITECTURE_HEADER 结构数组。不用于 x86 或 x64，但看来已用于 DEC/Compaq Alpha。
+	pe_IMAGE_DIRECTORY_ENTRY_GLOBALPTR    = 8 // 在某些架构体系上 VirtualAddress 域是一个 RVA，被用来作为全局指针（gp）。不用于 x86，而用于 IA-64。Size 域没有被使用。参见2000年11月的 Under The Hood 专栏可得到关于 IA-64 gp 的更多信息
+	pe_IMAGE_DIRECTORY_ENTRY_TLS          = 9 // 指向线程局部存储初始化节
 	pe_IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG  = 10 // 指向一个 IMAGE_LOAD_CONFIG_DIRECTORY 结构。IMAGE_LOAD_CONFIG_DIRECTORY 中的信息是特定于 Windows NT、Windows 2000 和 Windows XP 的(例如 GlobalFlag 值)。要把这个结构放到你的可执行文件中，你必须用名字 __load_config_used 定义一个全局结构，类型是 IMAGE_LOAD_CONFIG_DIRECTORY。对于非 x86 的其它体系，符号名是 _load_config_used (只有一个下划线)。如果你确实要包含一个 IMAGE_LOAD_CONFIG_DIRECTORY，那么在 C++ 中要得到正确的名字比较棘手。链接器看到的符号名必须是__load_config_used (两个下划线)。C++ 编译器会在全局符号前加一个下划线。另外，它还用类型信息修饰全局符号名
 	pe_IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT = 11 // 指向一个 IMAGE_BOUND_IMPORT_DESCRIPTOR 结构数组，对应于这个映像绑定的每个 DLL。数组元素中的时间戳允许加载器快速判断绑定是否是新的。如果不是，加载器忽略绑定信息并且按正常方式解决导入 API
 	pe_IMAGE_DIRECTORY_ENTRY_IAT          = 12 // 指向第一个导入地址表（IAT）的开始位置。对应于每个被导入 DLL 的 IAT 都连续地排列在内存中。Size 域指出了所有 IAT 的总的大小。在写入导入函数的地址时加载器使用这个地址和 Size 域指定的大小临时地标记 IAT 为可读写
