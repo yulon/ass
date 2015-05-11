e5f78d4b8e374308eb9587e4771ae4286d329ef6
Yuchen Wang <yulon@me.com>
2015-05-12 00:04:16 +0800

    Change pkg struct

--- labeler/labeler.go
+++ labeler/labeler.go
@@ -6,7 +6,7 @@ import (
 	"github.com/yulon/ass/bin"
 )
 
-type Writer struct{
+type Labeler struct{
 	ws io.WriteSeeker
 	labs map[string]int64
 	pits []pit
@@ -21,85 +21,85 @@ type pit struct{
 	ic bin.IntConv
 }
 
-func New(ws io.WriteSeeker) *Writer {
+func New(ws io.WriteSeeker) *Labeler {
 	offset, err := ws.Seek(0, 1)
 	if err != nil {
 		return nil
 	}
 
-	w := &Writer{
+	laber := &Labeler{
 		ws: ws,
 		labs: map[string]int64{},
 		pits: []pit{},
 		base: offset,
 	}
-	return w
+	return laber
 }
 
-func (w *Writer) Label(l string) error {
-	offset, err := w.ws.Seek(0, 1)
+func (laber *Labeler) Label(l string) error {
+	offset, err := laber.ws.Seek(0, 1)
 	if err != nil {
 		return err
 	}
 
-	w.labs[l] = offset
+	laber.labs[l] = offset
 	return nil
 }
 
-func (w *Writer) Pit(startLabel string, endLabel string, added int64, ic bin.IntConv) (int, error) {
-	addr, err := w.ws.Seek(0, 1)
+func (laber *Labeler) Pit(startLabel string, endLabel string, added int64, ic bin.IntConv) (int, error) {
+	addr, err := laber.ws.Seek(0, 1)
 	if err != nil {
 		return 0, err
 	}
 
-	w.pits = append(w.pits, pit{
+	laber.pits = append(laber.pits, pit{
 		addr: addr,
 		start: startLabel,
 		end: endLabel,
 		added: added,
 		ic: ic,
 	})
-	return w.ws.Write(ic(0))
+	return laber.ws.Write(ic(0))
 }
 
-func (w *Writer) Close() error {
-	current, err := w.ws.Seek(0, 1)
+func (laber *Labeler) Close() error {
+	current, err := laber.ws.Seek(0, 1)
 	if err != nil {
 		return err
 	}
 
-	for i := 0; i < len(w.pits); i++ {
+	for i := 0; i < len(laber.pits); i++ {
 		var start, end int64
 		var ok bool
 
-		if w.pits[i].start == "" {
-			start = w.base
+		if laber.pits[i].start == "" {
+			start = laber.base
 		}else{
-			start, ok = w.labs[w.pits[i].start]
+			start, ok = laber.labs[laber.pits[i].start]
 			if !ok {
-				return errors.New(w.pits[i].start + " is not found")
+				return errors.New(laber.pits[i].start + " is not found")
 			}
 		}
 
-		if w.pits[i].end == "" {
-			end = w.pits[i].addr
+		if laber.pits[i].end == "" {
+			end = laber.pits[i].addr
 		}else{
-			end, ok = w.labs[w.pits[i].end]
+			end, ok = laber.labs[laber.pits[i].end]
 			if !ok {
-				return errors.New(w.pits[i].end + " is not found")
+				return errors.New(laber.pits[i].end + " is not found")
 			}
 		}
 
-		n := end - start + w.pits[i].added
+		n := end - start + laber.pits[i].added
 
-		_, err = w.ws.Seek(w.pits[i].addr, 0)
+		_, err = laber.ws.Seek(laber.pits[i].addr, 0)
 		if err != nil {
 			return err
 		}
 
-		w.ws.Write(w.pits[i].ic(n))
+		laber.ws.Write(laber.pits[i].ic(n))
 	}
 
-	_, err = w.ws.Seek(current, 0)
+	_, err = laber.ws.Seek(current, 0)
 	return err
 }
