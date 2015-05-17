1a662df8fdcfb100633b78a84cd2ab6f994c503c
Yuchen Wang <yulon@me.com>
2015-05-17 20:26:50 +0800

    Rename

--- i386.go
+++ i386.go
@@ -7,14 +7,14 @@ import (
 	"github.com/yulon/go-bin"
 )
 
-type I386Writer struct{
+type I386 struct{
 	b *bin.Writer
 	l *octrl.Labeler
 	bva int64
 }
 
-func NewI386Writer(ws io.WriteSeeker, BaseVA int64) *I386Writer {
-	return &I386Writer{
+func NewI386(ws io.WriteSeeker, BaseVA int64) *I386 {
+	return &I386{
 		b: bin.NewWriter(ws),
 		l: octrl.NewLabeler(ws),
 		bva: BaseVA,
@@ -33,15 +33,15 @@ const (
 	ooReg = 3 // 11
 )
 
-func (w *I386Writer) Label(l string) {
+func (w *I386) Label(l string) {
 	w.l.Label(l)
 }
 
-func (w *I386Writer) Close() error {
+func (w *I386) Close() error {
 	return w.l.Close()
 }
 
-func (w *I386Writer) switchW(infa interface{}, conv bin.Converter) {
+func (w *I386) switchW(infa interface{}, conv bin.Converter) {
 	switch v := infa.(type){
 		case int:
 			w.b.Write(conv(v))
@@ -52,12 +52,12 @@ func (w *I386Writer) switchW(infa interface{}, conv bin.Converter) {
 	}
 }
 
-func (w *I386Writer) MovRegImm(dst int, src interface{}) {
+func (w *I386) MovRegImm(dst int, src interface{}) {
 	w.b.Byte(184 | dst) // 1011wrrr w=1 rrr=dst
 	w.switchW(src, bin.Dword)
 }
 
-func (w *I386Writer) MovRegMem(dst int, src interface{}, byteSrc uint8) {
+func (w *I386) MovRegMem(dst int, src interface{}, byteSrc uint8) {
 	if dst == EAX {
 		w.b.Byte(161)
 	}else{
@@ -66,22 +66,22 @@ func (w *I386Writer) MovRegMem(dst int, src interface{}, byteSrc uint8) {
 	w.switchW(src, bin.Dword)
 }
 
-func (w *I386Writer) MovRegReg(dst int, src int) {
+func (w *I386) MovRegReg(dst int, src int) {
 	w.b.WordB(35776 | ooReg << 6 | dst << 3 | src) // 1000101woorrrmmm w=1 oo=ooReg rrr=dst mmm=src
 }
 
-func (w *I386Writer) PushReg(src int) {
+func (w *I386) PushReg(src int) {
 	w.b.Byte(80 | src)
 }
 
-func (w *I386Writer) Pop(dst int) {
+func (w *I386) Pop(dst int) {
 	w.b.Byte(88 | dst)
 }
 
-func (w *I386Writer) CallReg(dst int) {
+func (w *I386) CallReg(dst int) {
 	w.b.WordB(65488 | ooReg << 6 | dst) // 11111111oo010mmm oo=ooReg mmm=dst
 }
 
-func (w *I386Writer) Lock() { // ADD, ADC, AND, BTC, BTR, BTS, CMPXCHG, CMPXCH8B, DEC, INC, NEG, NOT, OR, SBB, SUB, XOR, XADD, XCHG
+func (w *I386) Lock() { // ADD, ADC, AND, BTC, BTR, BTS, CMPXCHG, CMPXCH8B, DEC, INC, NEG, NOT, OR, SBB, SUB, XOR, XADD, XCHG
 	w.b.Byte(240)
 }
